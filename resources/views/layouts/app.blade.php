68d577ef9d77ba3cfa8d054522ed7db3cd3993c3
Griffin1610 <PollyGS14@uww.edu>
2025-11-02 22:52:58 -0600

    remove duplicate links

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,8 +10,6 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600,700" rel="stylesheet" />
-        <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
-
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
